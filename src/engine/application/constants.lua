4b27a26217ffa68b684dabdab40d9fe8c4000f3f
huulong <n.huu.long@gmail.com>
2019-06-05 21:42:04 +0100

    [CONSTANTS] Added 30fps constants

--- src/engine/application/constants.lua
+++ src/engine/application/constants.lua
@@ -1,7 +1,14 @@
+-- common pico-8 constants
+
+-- screen
 screen_width = 128
 screen_height = 128
 
+-- tilemap
 tile_size = 8
 
+-- time
 fps = 60
 delta_time = 1 / fps
+fps30 = 30
+delta_time30 = 1 / fps30
