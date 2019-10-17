963218476035d503885657138a9228f42ba21dc5
hsandt <hs@fuwar.in>
2019-10-18 00:14:45 +0100

    [UI] Added draw_gauge

--- src/engine/ui/ui.lua
+++ src/engine/ui/ui.lua
@@ -134,6 +134,50 @@ function ui.draw_rounded_box(x0, y0, x1, y1, border_color, fill_color)
   end
 end
 
+-- draw a gauge with frame between x0, y0, x1 and y1 (bottom to top and right to left arguments are supported)
+--   with a given fill ratio, filled toward given direction: directions
+function ui.draw_gauge(x0, y0, x1, y1, fill_ratio, fill_direction, border_color, background_color, fill_color)
+  -- if coordinates are not top to bottom and left to right, swap them so our calculations with fill are correct
+  if x0 > x1 then
+    local x = x0
+    x0 = x1
+    x1 = x
+  end
+  if y0 > y1 then
+    local y = y0
+    y0 = y1
+    y1 = y
+  end
+
+  -- draw border
+  rect(x0, y0, x1, y1, border_color)
+
+  -- fill rectangle if big enough to have an interior
+  if x0 + 1 <= x1 - 1 and y0 + 1 <= y1 - 1 then
+    rectfill(x0 + 1, y0 + 1, x1 - 1, y1 - 1, background_color)
+
+    if fill_direction == directions.left or fill_direction == directions.right then
+      local gauge_width = flr(fill_ratio * (x1 - x0 - 1))  -- padding of 1px each side
+      if gauge_width >= 1 then
+        if fill_direction == directions.right then
+          rectfill(x0 + 1, y0 + 1, x0 + gauge_width, y1 - 1, fill_color)
+        else  -- fill_direction == directions.left
+          rectfill(x1 - gauge_width, y0 + 1, x1 - 1, y1 - 1, fill_color)
+        end
+      end
+    else  -- vertical direction
+      local gauge_height = flr(fill_ratio * (y1 - y0 - 1))  -- padding of 1px each side
+      if gauge_height >= 1 then
+        if fill_direction == directions.down then
+          rectfill(x0 + 1, y0 + 1, x1 - 1, y0 + gauge_height, fill_color)
+        else  -- fill_direction == directions.up
+          rectfill(x0 + 1, y1 - gauge_height, x1 - 1, y1 - 1, fill_color)
+        end
+      end
+    end
+  end
+end
+
 -- label struct: container for a text to draw at a given position
 local label = new_struct()
 
--- src/engine/ui/ui_utest.lua
+++ src/engine/ui/ui_utest.lua
@@ -180,24 +180,21 @@ describe('ui', function ()
   describe('draw_box', function ()
 
     setup(function ()
-      stub(_G, "line")
       stub(_G, "rect")
       stub(_G, "rectfill")
     end)
 
     teardown(function ()
-      line:revert()
       rect:revert()
       rectfill:revert()
     end)
 
     after_each(function ()
-      line:clear()
       rect:clear()
       rectfill:clear()
     end)
 
-    it('should draw a rect for the border', function ()
+    it('should draw a rect for the frame', function ()
       ui.draw_box(10, 20, 40, 50, colors.black, colors.blue)
 
       local s = assert.spy(rect)
@@ -205,7 +202,7 @@ describe('ui', function ()
       s.was_called_with(10, 20, 40, 50, colors.black)
     end)
 
-    it('should fill a rect 1px inside', function ()
+    it('should fill a rect with padding 1px for the background', function ()
       ui.draw_box(10, 20, 40, 50, colors.black, colors.blue)
 
       local s = assert.spy(rectfill)
@@ -213,7 +210,7 @@ describe('ui', function ()
       s.was_called_with(11, 21, 39, 49, colors.blue)
     end)
 
-    it('should fill a rect 1px inside, supporting bottom and right coord first', function ()
+    it('should fill a rect with padding 1px, supporting bottom and right coord first', function ()
       ui.draw_box(40, 50, 10, 20, colors.black, colors.blue)
 
       local s = assert.spy(rectfill)
@@ -221,7 +218,7 @@ describe('ui', function ()
       s.was_called_with(11, 21, 39, 49, colors.blue)
     end)
 
-    it('should not fill a rect 1px inside if box is too small', function ()
+    it('should not fill a rect with padding 1px if box is too small', function ()
       ui.draw_box(10, 20, 40, 21, colors.black, colors.blue)
 
       local s = assert.spy(rectfill)
@@ -247,7 +244,7 @@ describe('ui', function ()
       rectfill:clear()
     end)
 
-    it('should draw a rect with 1px cut corners for the border', function ()
+    it('should draw a rect with 1px cut corners for the frame', function ()
       ui.draw_rounded_box(10, 20, 40, 50, colors.black, colors.blue)
 
       local s = assert.spy(line)
@@ -258,7 +255,7 @@ describe('ui', function ()
       s.was_called_with(10, 49, 10, 21, colors.black)
     end)
 
-    it('should fill a rect 1px inside', function ()
+    it('should fill a rect with padding 1px for the background', function ()
       ui.draw_rounded_box(10, 20, 40, 50, colors.black, colors.blue)
 
       local s = assert.spy(rectfill)
@@ -266,7 +263,7 @@ describe('ui', function ()
       s.was_called_with(11, 21, 39, 49, colors.blue)
     end)
 
-    it('should fill a rect 1px inside, supporting bottom and right coord first', function ()
+    it('should fill a rect with padding 1px, supporting bottom and right coord first', function ()
       ui.draw_rounded_box(40, 50, 10, 20, colors.black, colors.blue)
 
       local s = assert.spy(rectfill)
@@ -274,11 +271,105 @@ describe('ui', function ()
       s.was_called_with(11, 21, 39, 49, colors.blue)
     end)
 
-    it('should not fill a rect 1px inside if box is too small', function ()
+    it('should not fill a rect with padding 1px if box is too small', function ()
       ui.draw_rounded_box(10, 20, 40, 21, colors.black, colors.blue)
 
       local s = assert.spy(rectfill)
       s.was_not_called()
+    end)
+
+  end)
+
+  describe('draw_gauge', function ()
+
+    setup(function ()
+      stub(_G, "rect")
+      stub(_G, "rectfill")
+    end)
+
+    teardown(function ()
+      rect:revert()
+      rectfill:revert()
+    end)
+
+    after_each(function ()
+      rect:clear()
+      rectfill:clear()
+    end)
+
+    it('should draw a rect for the frame', function ()
+      ui.draw_gauge(10, 20, 40, 50, 0, directions.right, colors.black, colors.white, colors.blue)
+
+      local s = assert.spy(rect)
+      s.was_called(1)
+      s.was_called_with(10, 20, 40, 50, colors.black)
+    end)
+
+    it('should fill a rect with padding 1px for the background, but no extra fill if gauge is too small', function ()
+      ui.draw_gauge(10, 20, 40, 50, 0, directions.right, colors.black, colors.white, colors.blue)
+
+      local s = assert.spy(rectfill)
+      s.was_called(1)
+      s.was_called_with(11, 21, 39, 49, colors.white)
+    end)
+
+    it('should fill a rect with padding 1px, supporting bottom and right coord first, but no extra fill if gauge is too small', function ()
+      ui.draw_gauge(40, 50, 10, 20, 0, directions.right, colors.black, colors.white, colors.blue)
+
+      local s = assert.spy(rectfill)
+      s.was_called(1)
+      s.was_called_with(11, 21, 39, 49, colors.white)
+    end)
+
+    it('should fill a rect with padding 1px for the background, and extra fill matching fill ratio (toward right)', function ()
+      ui.draw_gauge(10, 20, 41, 51, 0.5, directions.left, colors.black, colors.white, colors.blue)
+
+      local s = assert.spy(rectfill)
+      s.was_called(2)
+      s.was_called_with(11, 21, 40, 50, colors.white)
+      s.was_called_with(26, 21, 40, 50, colors.blue)
+    end)
+
+    it('should fill a rect with padding 1px for the background, and extra fill matching fill ratio (toward right)', function ()
+      ui.draw_gauge(10, 20, 41, 51, 0.5, directions.right, colors.black, colors.white, colors.blue)
+
+      local s = assert.spy(rectfill)
+      s.was_called(2)
+      s.was_called_with(11, 21, 40, 50, colors.white)
+      s.was_called_with(11, 21, 25, 50, colors.blue)
+    end)
+
+    it('should fill a rect with padding 1px for the background, and extra fill matching fill ratio (toward down and inverted coords)', function ()
+      ui.draw_gauge(61, 71, 50, 20, 0.5, directions.down, colors.black, colors.white, colors.blue)
+
+      local s = assert.spy(rectfill)
+      s.was_called(2)
+      s.was_called_with(51, 21, 60, 70, colors.white)
+      s.was_called_with(51, 21, 60, 45, colors.blue)
+    end)
+
+    it('should fill a rect with padding 1px for the background, and extra fill matching fill ratio (toward up and inverted coords)', function ()
+      ui.draw_gauge(61, 71, 50, 20, 0.5, directions.up, colors.black, colors.white, colors.blue)
+
+      local s = assert.spy(rectfill)
+      s.was_called(2)
+      s.was_called_with(51, 21, 60, 70, colors.white)
+      s.was_called_with(51, 46, 60, 70, colors.blue)
+    end)
+
+    it('should fill a rect with padding 1px for the background, and full fill (toward down and inverted coords)', function ()
+      ui.draw_gauge(61, 71, 50, 20, 1, directions.down, colors.black, colors.white, colors.blue)
+
+      local s = assert.spy(rectfill)
+      s.was_called(2)
+      s.was_called_with(51, 21, 60, 70, colors.white)
+      s.was_called_with(51, 21, 60, 70, colors.blue)
+    end)
+
+    it('should not rectfill at all if box is too small', function ()
+      ui.draw_gauge(10, 20, 40, 21, 0, directions.right, colors.black, colors.white, colors.blue)
+
+      assert.spy(rectfill).was_not_called()
     end)
 
   end)
