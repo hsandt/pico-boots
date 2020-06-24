797cb0e1d9674fc4d353c500a697d67e3f14caff
Hyper Sonic <hs@fuwar.in>
2020-06-24 19:18:56 +0200

    [TEST] PICO-8 utests now print test name to console for easier debug

--- src/engine/test/unittest.lua
+++ src/engine/test/unittest.lua
@@ -21,6 +21,19 @@ end
 
 function utest_manager:run_all_tests()
   for utest in all(self.utests) do
+    -- In general, the assert message will be so long that the previous prints won't be visible
+    -- so instead, we print to console so in case of failure, we can check the last test
+    -- that started. This way, no need to give a meaningful assert message in each utest,
+    -- unless there are some interesting variables to print.
+    -- Of course, if we apply the suggestion below and track all tests before summing everything
+    -- up (like pico-test, but storing test state internally rather than streaming to output),
+    -- we could print all the test results properly to the console.
+    printh("start pico8 utest: "..utest.name)
+
+    -- For now, callback should test directly with assert which will stop at the first failure,
+    -- but it's not convenient to continue running other tests after a failure
+    -- to sum-up later, so consider making a custom verify function that
+    -- checks a boolean and if false, will print that the test failed later
     utest.callback()
   end
 end
