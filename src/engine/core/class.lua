696d592a663cc1eec5e8ae559b3b2d0892a44db9
huulong <n.huu.long@gmail.com>
2020-08-23 20:05:59 +0200

    [CLASS] !API break! Strip struct_eq from debug/release build (keep in busted and p8utest) to reduce tokens count You will need to replace struct comparison with == or ~= with custom member comparison, or custom __eq This is to prepare stripping struct_eq entirely, as luassert are_same is generally a better way to compare thanks to * showing in front of first difference in error message (only p8utests cannot use this, but use are_same_with_message instead which can be improved later)

--- src/engine/core/class.lua
+++ src/engine/core/class.lua
@@ -18,6 +18,7 @@ local function concat(lhs, rhs)
 --#endif
 end
 
+--#if p8utest
 -- metatable and memberwise equality comparison with usual equality operator
 -- (shallow or deep depending on override)
 -- return true iff tables have the same metatable and their members are equal
@@ -27,6 +28,7 @@ local function struct_eq(lhs, rhs)
   -- if you need deep content comparison for utest, just use unittest_helper > are_same
   return getmetatable(lhs) == getmetatable(rhs) and are_same_shallow(lhs, rhs)
 end
+--#endif
 
 -- return a copy of a struct instance 'self'
 -- this is a simplified version of deepcopy implementations and only support
@@ -162,7 +164,9 @@ function new_struct()
   local struct = {}
   struct.__index = struct  -- 1st struct as instance metatable
   struct.__concat = concat
+--#if p8utest
   struct.__eq = struct_eq
+--#endif
   struct.copy = copy
   struct.copy_assign = copy_assign
 
@@ -178,7 +182,9 @@ function derived_struct(base_struct)
   local derived = {}
   derived.__index = derived
   derived.__concat = concat
+--#if p8utest
   derived.__eq = struct_eq
+--#endif
 
   setmetatable(derived, {
     __index = base_struct,
