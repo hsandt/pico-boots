489aa93d2cd1976120483f6a2146ca280ac51f44
huulong <n.huu.long@gmail.com>
2020-11-15 21:24:34 +0100

    [UI] Overlay is now content-agnostic, only needs drawables

--- src/engine/ui/overlay.lua
+++ src/engine/ui/overlay.lua
@@ -1,70 +1,60 @@
-local label = require("engine/ui/label")
-
--- overlay class: allows to draw labels on top of the screen
+-- overlay class: allows to draw drawables on top of the screen
+-- a drawable struct is a struct that implements a draw method
 local overlay = new_class()
 
 -- state vars
--- labels      {string: label}  table of labels to draw, identified by name
+-- drawables      {string: drawable}  table of drawables to draw, identified by name
 function overlay:init()
-  self.labels = {}
+  self.drawables = {}
 end
 
 --#if tostring
 function overlay:_tostring()
-  local label_names = {}
+  local drawable_names = {}
   local pairs_callback = pairs
-  -- if #dump is used, print label names in alphabetical order
+  -- if #dump is used, print drawable names in alphabetical order
 --#if dump
   pairs_callback = orderedPairs
 --#endif
-  for name, _lab in pairs_callback(self.labels) do
-    add(label_names, '"'..name..'"')
+  for name, _lab in pairs_callback(self.drawables) do
+    add(drawable_names, '"'..name..'"')
   end
-  return "overlay(label names: {"..joinstr_table(", ", label_names).."})"
+  return "overlay(drawable names: {"..joinstr_table(", ", drawable_names).."})"
 end
 --#endif
 
--- add a label identified by a name, containing a text string,
+-- add a drawable identified by a name, containing a text string,
 -- at a position vector, with a given color
--- if a label with the same name already exists, replace it
-function overlay:add_label(name, text, position, colour, outline_colour)
-  if not colour then
-    colour = colors.black
-    warn("overlay:add_label no colour passed, will default to black (0)", 'ui')
-  end
-
-  local lab = label(text, position, colour, outline_colour)
-
-  if self.labels[name] == nil then
-    -- create new label and add it
-    self.labels[name] = lab
+-- if a drawable with the same name already exists, replace it
+function overlay:add_drawable(name, drawable)
+  if self.drawables[name] == nil then
+    -- create new drawable and add it
+    self.drawables[name] = drawable
   else
-    -- set existing label properties
-    self.labels[name]:copy_assign(lab)
+    -- set existing drawable properties
+    self.drawables[name]:copy_assign(drawable)
   end
-
-  return self.labels[name]
 end
 
--- remove a label identified by a name
--- if the label is not found, fails with warning
-function overlay:remove_label(name)
-  if self.labels[name] ~= nil then
-    self.labels[name] = nil
+-- remove a drawable identified by a name
+-- if the drawable is not found, fails with warning
+function overlay:remove_drawable(name)
+  if self.drawables[name] ~= nil then
+    self.drawables[name] = nil
   else
-    warn("overlay:remove_label: could not find label with name: '"..name.."'", 'ui')
+    warn("overlay:remove_drawable: could not find drawable with name: '"..name.."'", 'ui')
   end
 end
 
--- remove all the labels
-function overlay:clear_labels()
-  clear_table(self.labels)
+-- remove all the drawables
+function overlay:clear_drawables()
+  clear_table(self.drawables)
 end
 
--- draw all labels in the overlay. order is not guaranteed
-function overlay:draw_labels()
-  for _, label in pairs(self.labels) do
-    label:draw()
+-- draw all drawables in the overlay. order is not guaranteed
+function overlay:draw()
+  for _, drawable in pairs(self.drawables) do
+    drawable:draw()
   end
 end
 
--- src/engine/ui/overlay_utest.lua
+++ src/engine/ui/overlay_utest.lua
@@ -1,25 +1,34 @@
 require("engine/test/bustedhelper")
 local overlay = require("engine/ui/overlay")
-local label = require("engine/ui/label")
 
 describe('overlay', function ()
 
+  -- dummy drawable struct for demonstration
+  local dummy_drawable = new_struct()
+
+  function dummy_drawable:init(colour)
+    self.colour = colour
+  end
+
+  function dummy_drawable:draw()
+  end
+
   describe('init', function ()
 
-    it('should init overlay with empty labels sequence', function ()
-      assert.are_same({}, overlay().labels)
+    it('should init overlay with empty drawables sequence', function ()
+      assert.are_same({}, overlay().drawables)
     end)
 
   end)
 
   describe('_tostring', function ()
 
-    it('should return "overlay(X label(s))"', function ()
+    it('should return "overlay(drawable names: {...})"', function ()
       local overlay_instance = overlay()
-      overlay_instance.labels = {["title"] = "dummy label", hud = "dummy label"}
+      overlay_instance.drawables = {["title"] = "dummy drawable", hud = "dummy drawable"}
       -- thx to #dump being always present in busted utests, we guarantee ordered pairs
       -- so we can test keys in alphabetical order
-      assert.are_equal('overlay(label names: {"hud", "title"})', overlay_instance:_tostring())
+      assert.are_equal('overlay(drawable names: {"hud", "title"})', overlay_instance:_tostring())
     end)
 
   end)
@@ -32,67 +41,53 @@ describe('overlay', function ()
       overlay_instance = overlay()
     end)
 
-    describe('(no labels)', function ()
+    describe('(no drawables)', function ()
 
       teardown(function ()
-        clear_table(overlay_instance.labels)
+        clear_table(overlay_instance.drawables)
       end)
 
-      describe('add_label', function ()
+      describe('add_drawable', function ()
 
-        local warn_stub
-
-        setup(function ()
-          warn_stub = stub(_G, "warn")
-        end)
-
-        teardown(function ()
-          warn_stub:revert()
-        end)
-
-        after_each(function ()
-          warn_stub:clear()
-        end)
-
-        it('should add a new label and return it', function ()
-          local result = overlay_instance:add_label("test", "content", vector(2, 4), colors.red, colors.white)
-          assert.are_same(label("content", vector(2, 4), colors.red, colors.white), overlay_instance.labels["test"])
-          assert.are_equal(overlay_instance.labels["test"], result)
-        end)
-
-        it('should add a new black label with warning if no colour is passed', function ()
-          overlay_instance:add_label("test", "content", vector(2, 4))
-          assert.spy(warn_stub).was_called(1)
-          assert.spy(warn_stub).was_called_with('overlay:add_label no colour passed, will default to black (0)', 'ui')
-          assert.are_same(label("content", vector(2, 4), colors.black), overlay_instance.labels["test"])
+        it('should add a new drawable and return it', function ()
+          local mock = dummy_drawable(colors.white)
+          overlay_instance:add_drawable("test", mock)
+          assert.are_same(dummy_drawable(colors.white), overlay_instance.drawables["test"])
+          assert.are_equal(overlay_instance.drawables["test"], mock)
         end)
 
       end)
 
     end)
 
-    describe('(label "mock" and "mock2" added)', function ()
+    describe('(drawable "mock" and "mock2" added)', function ()
+
+      local mock
 
       before_each(function ()
-        overlay_instance:add_label("mock", "mock content", vector(1, 1), colors.blue)
-        overlay_instance:add_label("mock2", "mock content 2", vector(2, 2), colors.dark_purple)
+        mock = dummy_drawable(colors.blue)
+        overlay_instance:add_drawable("mock", mock)
+        overlay_instance:add_drawable("mock2", dummy_drawable(colors.dark_purple))
       end)
 
       after_each(function ()
-        clear_table(overlay_instance.labels)
+        clear_table(overlay_instance.drawables)
       end)
 
-      describe('add_label', function ()
+      describe('add_drawable', function ()
 
-        it('should replace an existing label', function ()
-          local result = overlay_instance:add_label("mock", "mock content 2", vector(3, 7), colors.white, colors.yellow)
-          assert.are_same(label("mock content 2", vector(3, 7), colors.white, colors.yellow), overlay_instance.labels["mock"])
-          assert.are_equal(overlay_instance.labels["mock"], result)
+        it('should replace an existing drawable\'s content while preserving the reference', function ()
+          local mock3 = dummy_drawable(colors.yellow)
+          overlay_instance:add_drawable("mock", mock3)
+          assert.are_same(dummy_drawable(colors.yellow), overlay_instance.drawables["mock"])
+          assert.are_same(mock3, overlay_instance.drawables["mock"])  -- content is same
+          assert.are_not_equal(mock3, overlay_instance.drawables["mock"])  -- reference is not same, mock3 was thrown away
+          assert.are_equal(mock, overlay_instance.drawables["mock"])  -- this proves the mock object reference didn't change
         end)
 
       end)
 
-      describe('remove_label', function ()
+      describe('remove_drawable', function ()
 
         local warn_stub
 
@@ -108,51 +103,50 @@ describe('overlay', function ()
           warn_stub:clear()
         end)
 
-        it('should remove an existing label', function ()
-          overlay_instance:remove_label("mock")
-          assert.is_nil(overlay_instance.labels["mock"])
+        it('should remove an existing drawable', function ()
+          overlay_instance:remove_drawable("mock")
+          assert.is_nil(overlay_instance.drawables["mock"])
         end)
 
-        it('should warn if the label name is not found', function ()
-          overlay_instance:remove_label("test")
+        it('should warn if the drawable name is not found', function ()
+          overlay_instance:remove_drawable("test")
           assert.spy(warn_stub).was_called(1)
-          assert.spy(warn_stub).was_called_with('overlay:remove_label: could not find label with name: \'test\'', 'ui')
-          assert.is_nil(overlay_instance.labels["test"])
+          assert.spy(warn_stub).was_called_with('overlay:remove_drawable: could not find drawable with name: \'test\'', 'ui')
+          assert.is_nil(overlay_instance.drawables["test"])
         end)
 
       end)
 
-      describe('clear_labels', function ()
+      describe('clear_drawables', function ()
 
-        it('should clear any existing label', function ()
-          overlay_instance:clear_labels()
-          return is_empty(overlay_instance.labels)
+        it('should clear any existing drawable', function ()
+          overlay_instance:clear_drawables()
+          return is_empty(overlay_instance.drawables)
         end)
 
       end)
 
-      describe('draw_labels', function ()
+      describe('draw_drawables', function ()
 
         setup(function ()
-          stub(label, "draw")
+          stub(dummy_drawable, "draw")
         end)
 
         teardown(function ()
-          label.draw:revert()
+          dummy_drawable.draw:revert()
         end)
 
-        it('should call label draw on each label', function ()
-          overlay_instance:draw_labels()
+        it('should call drawable draw on each drawable', function ()
+          overlay_instance:draw()
 
-          local s = assert.spy(label.draw)
-          s.was_called(2)
-          s.was_called_with(match.ref(overlay_instance.labels["mock"]))
-          s.was_called_with(match.ref(overlay_instance.labels["mock2"]))
+          assert.spy(dummy_drawable.draw).was_called(2)
+          assert.spy(dummy_drawable.draw).was_called_with(match.ref(overlay_instance.drawables["mock"]))
+          assert.spy(dummy_drawable.draw).was_called_with(match.ref(overlay_instance.drawables["mock2"]))
         end)
 
       end)
 
-    end)  -- (label "mock" and "mock2" added)
+    end)  -- (drawable "mock" and "mock2" added)
 
   end)  -- (overlay instance)
 
