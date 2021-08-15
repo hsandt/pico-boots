bdf8b9dd861d56b84d708a4ebc40727c9031906e
huulong <n.huu.long@gmail.com>
2021-08-15 20:19:05 +0200

    [HOT PATCH][WEB] Generalized regex patterns in patch_pico8_js.py to support pico8 0.2.2 (at least 0.2.2c) and hopefully future versions too

--- scripts/patch_pico8_js.py
+++ scripts/patch_pico8_js.py
@@ -6,19 +6,32 @@ import os
 import re
 
 
-# This script patches web assembly in an exported HTML cartridge for PICO-8 v0.2.1b,
+# This script patches web assembly in an exported HTML cartridge for PICO-8 v0.2.1-0.2.2,
 # doing operations equivalent to the patches used in patch_pico8_runtime.sh
 # - 4x token: extend cartridge token limit to 32768
 # - fast reload: skip loading animation (rotating floppy disk) during reload()
-# - fast load (Windows only): skip loading animation (rotating floppy disk) during load() (once)
+# - fast load (Windows only, TODO): skip loading animation (rotating floppy disk) during load() (once)
 
 
 wasm_funcs_pattern = re.compile(r"// EMSCRIPTEN_START_FUNCS\n(?:.*\n)*// EMSCRIPTEN_END_FUNCS")
 
-# The reload pattern sets some counter to 0x1e, and has actually two occurrences in WASM
+# The reload pattern sets some counter to 0x1e = 30, and has actually two occurrences in WASM
 # The second one must match either some BBS download-reload or the load-reload code,
 # but certainly replacing both can only help make loading faster
-reload_set_counter_pattern = re.compile(r";g=30-\(c\[732417\]\|0\).*c\[732417\]=28")
+# (note that the case of calling reload without passing a filename is not consistently patched
+# to be faster by our change, at least in the editor binary, so there may be even more calls
+# to change to improve the patch)
+# Regex pattern was updated to support both pico8 0.2.1 and 0.2.2, and hopefully further versions
+# too, assuming the variable names don't change but the numbers do.
+# If 0.2.3+ change variable names, please adapt the regex pattern.
+# We will entirely remove the matched statement.
+# Ex of first occurrence (pico8 0.2.2c):
+#    {Lm(2968664,b)|0;g=30-(c[742431]|0)|0;h=(g|0)<30?g:30;c[742430]=(h|0)>2?h:2;c[742431]=150}
+# => {Lm(2968664,b)|0}
+# Ex of second occurrence (pico8 0.2.2c):
+# {...;a[2968664]=0;g=30-(c[742431]|0)|0;h=(g|0)<30?g:30;c[742430]=(h|0)>2?h:2;c[742431]=150;return}
+# {...;a[2968664]=0;return}
+reload_set_counter_pattern = re.compile(r";g=30-\(c\[([0-9]+)\]\|0\).*c\[\1\]=[0-9]+")
 
 
 def get_wasm_funcs_string_patched_with_4x_token(wasm_funcs_string):
@@ -26,7 +39,14 @@ def get_wasm_funcs_string_patched_with_4x_token(wasm_funcs_string):
     Return a WASM string patched for 4x token
 
     """
-    return wasm_funcs_string.replace('if((o|0)>8192)', 'if((o|0)>32768)')
+    # Ex from pico8 0.2.2c:
+    #     o=gh(n)|0;if((o|0)>8192){ug(49924,6);c[i>>2]=o;
+    # =>  o=gh(n)|0;if((o|0)>32768){ug(49924,6);c[i>>2]=o;
+    # So far, the pattern worked on all recent versions of PICO-8
+    replaced_string = wasm_funcs_string.replace('if((o|0)>8192)', 'if((o|0)>32768)')
+    if replaced_string == wasm_funcs_string:
+        raise Exception(f"4x_token patch replacement found no matching string in WASM, patch could not be applied!")
+    return replaced_string
 
 
 def get_wasm_funcs_string_patched_with_fast_load(wasm_funcs_string):
@@ -45,7 +65,10 @@ def get_wasm_funcs_string_patched_with_fast_reload(wasm_funcs_string):
     """
     # remove counter set code completely (1st one is important for reload,
     # 2nd one may help for other loading procedures)
-    return reload_set_counter_pattern.sub('', wasm_funcs_string, count=2)
+    replaced_string = reload_set_counter_pattern.sub('', wasm_funcs_string, count=2)
+    if replaced_string == wasm_funcs_string:
+        raise Exception(f"4x_token patch replacement found no matching string in WASM, patch could not be applied!")
+    return replaced_string
 
 
 def parse_wasm_funcs_string(js_text):
@@ -92,7 +115,7 @@ def patch_js_file(input_filepath, output_filepath):
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    parser = argparse.ArgumentParser(description='Patch PICO-8 v0.2.1b JS file.')
+    parser = argparse.ArgumentParser(description='Patch PICO-8 v0.2.1-0.2.2 JS file.')
     parser.add_argument('input_filepath', type=str, help='path to JS file with WASM to patch')
     parser.add_argument('output_filepath', type=str, help='output path to patched JS file to create (pass same value as input_filepath to patch in-place)')
     args = parser.parse_args()
--- scripts/test_patch_pico8_js.py
+++ scripts/test_patch_pico8_js.py
@@ -9,7 +9,7 @@ import tempfile
 
 class TestPatchString(unittest.TestCase):
 
-    def test_get_wasm_funcs_string_patched_with_4x_token(self):
+    def test_get_wasm_funcs_string_patched_with_4x_token_pico8_0_2_1b(self):
         test_wasm = """// EMSCRIPTEN_START_FUNCS
         o=dh(n)|0;if((o|0)>8192){sg(47660,6);c[i>>2]=o}
 
@@ -25,21 +25,37 @@ class TestPatchString(unittest.TestCase):
         self.assertEqual(patch_pico8_js.get_wasm_funcs_string_patched_with_4x_token(test_wasm),
                          expected_patched_wasm)
 
-    def test_get_wasm_funcs_string_patched_with_fast_load(self):
+    def test_get_wasm_funcs_string_patched_with_4x_token_pico8_0_2_2c(self):
         test_wasm = """// EMSCRIPTEN_START_FUNCS
-        {rm(2928608,b)|0;g=30-(c[732417]|0)|0;h=(g|0)<30?g:30;c[732416]=(h|0)>0?h:0;c[732417]=28}
+        o=gh(n)|0;if((o|0)>8192){ug(49924,6);c[i>>2]=o;
 
 // EMSCRIPTEN_END_FUNCS
 """
 
         expected_patched_wasm = """// EMSCRIPTEN_START_FUNCS
-        {rm(2928608,b)|0}
+        o=gh(n)|0;if((o|0)>32768){ug(49924,6);c[i>>2]=o;
 
 // EMSCRIPTEN_END_FUNCS
 """
 
-        self.assertEqual(patch_pico8_js.get_wasm_funcs_string_patched_with_fast_load(test_wasm),
+        self.assertEqual(patch_pico8_js.get_wasm_funcs_string_patched_with_4x_token(test_wasm),
                          expected_patched_wasm)
+
+    def test_get_wasm_funcs_string_patched_with_4x_token_hypothetical_incompatible_future_version(self):
+        # let's imagine variable o -> p
+        test_wasm = """// EMSCRIPTEN_START_FUNCS
+        o=gh(n)|0;if((p|0)>8192){ug(49924,6);c[i>>2]=o;
+
+// EMSCRIPTEN_END_FUNCS
+"""
+
+        expected_patched_wasm = """// EMSCRIPTEN_START_FUNCS
+        o=gh(n)|0;if((p|0)>32768){ug(49924,6);c[i>>2]=o;
+
+// EMSCRIPTEN_END_FUNCS
+"""
+
+        self.assertRaises(Exception, patch_pico8_js.get_wasm_funcs_string_patched_with_4x_token, test_wasm)
 
     # TODO
     def test_get_wasm_funcs_string_patched_with_fast_load(self):
@@ -55,6 +71,60 @@ class TestPatchString(unittest.TestCase):
 
         self.assertEqual(patch_pico8_js.get_wasm_funcs_string_patched_with_fast_load(test_wasm),
                          expected_patched_wasm)
+
+
+    def test_get_wasm_funcs_string_patched_with_fast_reload_pico8_0_2_1b(self):
+        test_wasm = """// EMSCRIPTEN_START_FUNCS
+        {rm(2928608,b)|0;g=30-(c[732417]|0)|0;h=(g|0)<30?g:30;c[732416]=(h|0)>0?h:0;c[732417]=28}
+
+// EMSCRIPTEN_END_FUNCS
+"""
+
+        expected_patched_wasm = """// EMSCRIPTEN_START_FUNCS
+        {rm(2928608,b)|0}
+
+// EMSCRIPTEN_END_FUNCS
+"""
+
+        self.assertEqual(patch_pico8_js.get_wasm_funcs_string_patched_with_fast_reload(test_wasm),
+                         expected_patched_wasm)
+
+
+    def test_get_wasm_funcs_string_patched_with_fast_reload_pico8_0_2_2c(self):
+        test_wasm = """// EMSCRIPTEN_START_FUNCS
+        {Lm(2968664,b)|0;g=30-(c[742431]|0)|0;h=(g|0)<30?g:30;c[742430]=(h|0)>2?h:2;c[742431]=150}
+        {...;a[2968664]=0;g=30-(c[742431]|0)|0;h=(g|0)<30?g:30;c[742430]=(h|0)>2?h:2;c[742431]=150;return}
+
+// EMSCRIPTEN_END_FUNCS
+"""
+
+        expected_patched_wasm = """// EMSCRIPTEN_START_FUNCS
+        {Lm(2968664,b)|0}
+        {...;a[2968664]=0;return}
+
+// EMSCRIPTEN_END_FUNCS
+"""
+
+        self.assertEqual(patch_pico8_js.get_wasm_funcs_string_patched_with_fast_reload(test_wasm),
+                         expected_patched_wasm)
+
+    def test_get_wasm_funcs_string_patched_with_fast_reload_hypothetical_incompatible_future_version(self):
+        # let's imagine variable g -> h
+        test_wasm = """// EMSCRIPTEN_START_FUNCS
+        {Lm(2968664,b)|0;h=30-(c[742431]|0)|0;h=(g|0)<30?g:30;c[742430]=(h|0)>2?h:2;c[742431]=150}
+        {...;a[2968664]=0;h=30-(c[742431]|0)|0;h=(g|0)<30?g:30;c[742430]=(h|0)>2?h:2;c[742431]=150;return}
+
+// EMSCRIPTEN_END_FUNCS
+"""
+
+        expected_patched_wasm = """// EMSCRIPTEN_START_FUNCS
+        {Lm(2968664,b)|0}
+        {...;a[2968664]=0;return}
+
+// EMSCRIPTEN_END_FUNCS
+"""
+
+        self.assertRaises(Exception, patch_pico8_js.get_wasm_funcs_string_patched_with_fast_reload, test_wasm)
 
 
 class TestPatchFile(unittest.TestCase):
