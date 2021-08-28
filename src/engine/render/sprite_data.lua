0078d0543edb24b5d3e89834b0f813ce2b7eec46
huulong <n.huu.long@gmail.com>
2021-08-28 17:45:13 +0200

    [SPRITE DATA] Added scale parameter - ignored unless #sprite_scale - not compatible with angle

--- src/engine/render/sprite_data.lua
+++ src/engine/render/sprite_data.lua
@@ -34,12 +34,29 @@ end
 --#endif
 
 -- draw this sprite at position, optionally flipped
--- position  vector
--- flip_x    bool (nil ok, interpreted as false)
--- flip_y    bool (nil ok, interpreted as false)
--- angle     angle multiple of 0.25 (90 degrees), between 0 and 1 (excluded) (nil defaults to 0)
-function sprite_data:render(position, flip_x, flip_y, angle)
-  spr_r90(self.id_loc.i, self.id_loc.j, position.x, position.y, self.span.i, self.span.j, flip_x, flip_y, self.pivot.x, self.pivot.y, angle, self.transparent_color_bitmask)
+-- position                  vector
+-- flip_x                    bool (nil ok, interpreted as false)
+-- flip_y                    bool (nil ok, interpreted as false)
+-- angle                     angle multiple of 0.25 (90 degrees), between 0 and 1 (excluded) (default: 0)
+-- scale (#sprite_scale)     scale to draw at (default: 1)
+function sprite_data:render(position, flip_x, flip_y, angle, scale)
+-- scaling is stripped unless #sprite_scale to allow cartridges that don't need it to be minimal
+--#if sprite_scale
+  if scale and scale ~= 1 then
+    assert(not angle or angle % 1 == 0, "sprite_data:render: doesn't support both angle "..angle.." and scale "..scale)
+
+    local sw = 8 * self.span.i
+    local sh = 8 * self.span.j
+
+    palt(self.transparent_color_bitmask)
+    sspr(8 * self.id_loc.i, 8 * self.id_loc.j, sw, sh, position.x - scale * self.pivot.x, position.y - scale * self.pivot.y, scale * sw, scale * sh, flip_x, flip_y)
+    palt()
+  else
+--#endif
+    spr_r90(self.id_loc.i, self.id_loc.j, position.x, position.y, self.span.i, self.span.j, flip_x, flip_y, self.pivot.x, self.pivot.y, angle, self.transparent_color_bitmask)
+--#if sprite_scale
+  end
+--#endif
 end
 
 return sprite_data
--- src/engine/render/sprite_data_utest.lua
+++ src/engine/render/sprite_data_utest.lua
@@ -54,21 +54,51 @@ describe('sprite', function ()
 
     setup(function ()
       stub(_G, "spr_r90")
+      stub(_G, "sspr")
+      stub(_G, "palt")
     end)
 
     teardown(function ()
       spr_r90:revert()
+      sspr:revert()
+      palt:revert()
     end)
 
     after_each(function ()
       spr_r90:clear()
+      sspr:clear()
+      palt:clear()
     end)
 
-    it('should delegate rendering to spr_r90', function ()
+    it('(no scale) should delegate rendering to spr_r90', function ()
       spr_data:render(vector(41, 80), true, false, 0.5)
 
       assert.spy(spr_r90).was_called(1)
       assert.spy(spr_r90).was_called_with(1, 3, 41, 80, 2, 3, true, false, 11, 10, 0.5, spr_data.transparent_color_bitmask)
+    end)
+
+    it('(scale == 1) should delegate rendering to spr_r90', function ()
+      spr_data:render(vector(41, 80), true, false, 0.5, 1)
+
+      assert.spy(spr_r90).was_called(1)
+      assert.spy(spr_r90).was_called_with(1, 3, 41, 80, 2, 3, true, false, 11, 10, 0.5, spr_data.transparent_color_bitmask)
+    end)
+
+    it('(scale ~= 1) should delegate rendering to sspr with palt', function ()
+      spr_data:render(vector(41, 80), true, false, 0, 2)
+
+      assert.spy(sspr).was_called(1)
+      assert.spy(sspr).was_called_with(8 * 1, 8 * 3, 8 * 2, 8 * 3, 41 - 2 * 11, 80 - 2 * 10, 2 * 8 * 2, 2 * 8 * 3, true, false)
+
+      assert.spy(palt).was_called(2)
+      assert.spy(palt).was_called_with()  -- before sspr, but cannot verify
+      assert.spy(palt).was_called_with(spr_data.transparent_color_bitmask)  -- after sspr, but cannot verify
+    end)
+
+    it('(angle ~= 0 [1] and scale ~= 1) should assert', function ()
+      assert.has_error(function ()
+        spr_data:render(vector(41, 80), true, false, 0.5, 2)
+      end)
     end)
 
   end)
