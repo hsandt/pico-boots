431e004e2c34f39ff4b8793a67639092873c3f6e
huulong <n.huu.long@gmail.com>
2022-04-11 15:43:57 +0200

    [STRING] Added unit test for string_split `strspl` for empty string collapse

--- src/engine/core/string_split.lua
+++ src/engine/core/string_split.lua
@@ -1,23 +1,23 @@
--- port of lua string.split(string, separator)
+-- Split a string s into sub-strings separated by sep, and return the sequence of sub-strings
 -- ! prefer using PICO-8 v0.2.1's split when possible, i.e. you don't need
 -- ! table multi-separators nor collapse behavior (this will spare you string_split include
 -- ! and therefore precious characters)
 -- separator must be either:
 --  - a single character
 --  - a sequence of single characters
+-- parameter collapse:
+--  if true, collapse consecutive separators into a big one
+--  if false or nil, handle each separator separately,
+--   adding an empty string between each consecutive pair
 -- note: we prefer using a sequence of single characters {',', ';'} rather than
 --  a string coumpounded of the single char separators ",;", because
 --  we have a sequence `contains` method but no string `contains` method
 --  and also in case we want to add multi-char separator strings later
--- added parameter collapse:
---  if true, collapse consecutive separators into a big one
---  if false or nil, handle each separator separately,
---   adding an empty string between each consecutive pair
--- ex1: strspl("|a||b", '|')       => {"", "a", "", "b"}
--- ex2: strspl("|a||b", '|', true) => {"a", "b"}
+-- ex1: strspl("|a||b", '|')        => {"", "a", "", "b"}
+-- ex2: strspl("|a||b", '|', true)  => {"a", "b"}
 -- ex2: strspl("a,b;c", {',', ";"}) => {"a", "b", "c"}
 function strspl(s, sep, collapse)
-  local ret = {}
+  local sub_strings = {}
   local buffer = ""
 
   for i = 1, #s do
@@ -28,16 +28,26 @@ function strspl(s, sep, collapse)
     --  like PICO-8 v0.2.1b's split (this function was created before split was added)
     local is_sep = type(sep) == "table" and contains(sep, c) or c == sep
     if is_sep then
+      -- detected separator
       if #buffer > 0 or not collapse then
-        add(ret, buffer)
+        -- split here, add sub-string obtained so far to sub_strings sequence
+        add(sub_strings, buffer)
         buffer = ""
       end
     else
+      -- concatenate current character to sub-string buffer
       buffer = buffer..c
     end
   end
+
+  -- if some characters are left in sub-string buffer, we should add them
+  --  as final element in the sub-string sequence
+  -- if the buffer is empty (possible if s was empty, or s ended with a separator character)
+  --  and we are not collapsing, then we must still add this empty sub-string to the end
+  --  of the sequence
   if #buffer > 0 or not collapse then
-    add(ret, buffer)
+    add(sub_strings, buffer)
   end
-  return ret
+
+  return sub_strings
 end
