2c63148acd82d1c3702b643dd0616921ab972552
huulong <n.huu.long@gmail.com>
2022-10-31 18:53:52 +0100

    [LABEL] (! API BREAKING !) Support alignment. Alignment was inserted between position and color parameters
    so existing projects must update all label constructions.

--- src/engine/ui/label.lua
+++ src/engine/ui/label.lua
@@ -1,17 +1,19 @@
-local outline = require("engine/ui/outline")
+local text_helper = require("engine/ui/text_helper")
 
 -- Label struct: container for a text to draw at a given position
 -- Implements drawable:
 local label = new_struct()
 
--- text      printable    text content to draw (mainly string or number)
--- position  vector       position to draw the label at
--- colour    colors       color index to draw the label with
--- colour    (colors|-1)  color index to draw the label outline with. If -1, no outline is drawn
-function label:init(text, position, colour, outline_colour)
+-- text             printable    text content to draw (mainly string or number)
+-- position         vector       position to draw the label at
+-- alignment        alignments   text alignment
+-- colour           colors       color index to draw the label with
+-- outline_colour   (colors|-1)  color index to draw the label outline with. If -1, no outline is drawn
+function label:init(text, position, alignment, colour, outline_colour, override_char_width, override_char_height)
   assert(text)
   self.text = text
   self.position = position
+  self.alignment = alignment
   self.colour = colour
   self.outline_colour = outline_colour or -1  -- -1 is better than nil for copy_assign
 end
@@ -19,7 +21,7 @@ end
 --#if tostring
 function label:_tostring()
   local outline_colour_string = self.outline_colour >= 0 and color_tostring(self.outline_colour) or "none"
-  return "label('"..self.text.."' @ "..self.position.." in "..color_tostring(self.colour).." outlined "..outline_colour_string..")"
+  return "label('"..self.text.."' @ "..self.position.." aligned "..self.alignment.." in "..color_tostring(self.colour).." outlined "..outline_colour_string..")"
 end
 --#endif
 
@@ -27,7 +29,7 @@ function label:draw()
   -- be careful, as label struct prefers POD outline_colour of -1 for no outline,
   --  while print_with_outline will check for falsy values for no outline
   -- `or nil` is optional since false is also falsy, but clearer
-  outline.print_with_outline(self.text, self.position.x, self.position.y,
+  text_helper.print_aligned(self.text, self.position.x, self.position.y, self.alignment,
     self.colour, self.outline_colour >= 0 and self.outline_colour or nil)
 end
 
--- src/engine/ui/label_utest.lua
+++ src/engine/ui/label_utest.lua
@@ -1,13 +1,15 @@
 require("engine/test/bustedhelper")
 local label = require("engine/ui/label")
 
+local text_helper = require("engine/ui/text_helper")
+
 describe('label', function ()
 
   describe('init', function ()
 
     it('should init label with layer', function ()
-      local lab = label("great", vector(24, 68), colors.red, colors.yellow)
-      assert.are_same({"great", vector(24, 68), colors.red, colors.yellow}, {lab.text, lab.position, lab.colour, lab.outline_colour})
+      local lab = label("great", vector(24, 68), alignments.left, colors.red, colors.yellow)
+      assert.are_same({"great", vector(24, 68), alignments.left, colors.red, colors.yellow}, {lab.text, lab.position, lab.alignment, lab.colour, lab.outline_colour})
     end)
 
   end)
@@ -15,7 +17,7 @@ describe('label', function ()
   describe('_tostring', function ()
 
     it('should return "label(\'[text]\' @ [position] in [colour] outlined [outline_colour])"', function ()
-      assert.are_equal("label('good' @ vector(22, 62) in yellow outlined none)", label("good", vector(22, 62), colors.yellow + 16):_tostring())
+      assert.are_equal("label('good' @ vector(22, 62) aligned 1 in yellow outlined none)", label("good", vector(22, 62), alignments.left, colors.yellow + 16):_tostring())
     end)
 
   end)
@@ -23,39 +25,33 @@ describe('label', function ()
   describe('draw', function ()
 
     setup(function ()
-      stub(api, "print")
+      stub(text_helper, "print_aligned")
     end)
 
     teardown(function ()
-      api.print:revert()
+      text_helper.print_aligned:revert()
     end)
 
     after_each(function ()
-      api.print:clear()
+      text_helper.print_aligned:clear()
     end)
 
-    it('(no outline) should call print once', function ()
-      local lab = label("great", vector(24, 68), colors.red)
+    it('(no outline) should call print_aligned once without outline color (nil)', function ()
+      local lab = label("great", vector(24, 68), alignments.left, colors.red)
 
       lab:draw()
 
-      assert.spy(api.print).was_called(1)
-      assert.spy(api.print).was_called_with("great", 24, 68, colors.red)
+      assert.spy(text_helper.print_aligned).was_called(1)
+      assert.spy(text_helper.print_aligned).was_called_with("great", 24, 68, alignments.left, colors.red, nil)
     end)
 
-    it('(outline) should call print 4 times for outline and once for fill color', function ()
-      local lab = label("great", vector(24, 68), colors.red, colors.green)
+    it('(outline) should call print_aligned once with outline color', function ()
+      local lab = label("great", vector(24, 68), alignments.right, colors.red, colors.green)
 
       lab:draw()
 
-      assert.spy(api.print).was_called(5)
-
-      assert.spy(api.print).was_called_with("great", 23, 68, colors.green)
-      assert.spy(api.print).was_called_with("great", 25, 68, colors.green)
-      assert.spy(api.print).was_called_with("great", 24, 67, colors.green)
-      assert.spy(api.print).was_called_with("great", 24, 69, colors.green)
-
-      assert.spy(api.print).was_called_with("great", 24, 68, colors.red)
+      assert.spy(text_helper.print_aligned).was_called(1)
+      assert.spy(text_helper.print_aligned).was_called_with("great", 24, 68, alignments.right, colors.red, colors.green)
     end)
 
   end)
